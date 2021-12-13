c17b3ada2c6bd011ed7b9381e71b1eccacfcfaa1
Proph7000 <87013694+Proph7000@users.noreply.github.com>
2021-12-13 18:32:11 +0100

    Added my own logic

--- js/main.js
+++ js/main.js
@@ -1,26 +1,65 @@
 $(document).ready(function () {
   var currentFloor = 2;
-  var floorPath = $('.home-image path')
+  var floorPath = $('.home-image path');
   var counterUp = $('.counter-up');
   var counterDown = $('.counter-down');
   var modal = $('.modal');
   var modalCloseButton = $('.modal-close-button');
   var viewFlatsButton = $('.view-flats');
   var currentFlats = 1;
+  var flatsPath = $('.modal-image path');
+  //var flatLink = $('.flat-link');
+  var flat;
+  var previousFlat = $(`.flat${currentFlats}`);
+  var flatLink = $('.flat-link');
+  let numberFlats = document.querySelectorAll('.number-flat');
   
   floorPath.on('mouseover', function() {
     currentFloor = $(this).attr('data-floor');
     $('.counter').text(currentFloor);
     floorPath.removeClass('current-floor');
+    //console.log(currentFloor);
   });
   
-  /*flatsPath.on('mouseover', function() {
+  flatsPath.on('mouseover', function() {
     currentFlats = $(this).attr('data-flats');
-  });*/
+    previousFlat.removeClass('underline');
+    flat = $(`.flat${currentFlats}`);
+    flat.toggleClass('underline');
+    previousFlat = flat;
+  });
 
-  floorPath.on('click', toggleModal);
+  flatsPath.on('mouseout', function() {
+    flat.toggleClass('underline');
+  });
+
+  flatLink.on('mouseover', function() {
+    currentFlats = $(this).attr('data-flat');
+    flatsPath.removeClass('visible');
+    $(`[data-flats=${currentFlats}]`).toggleClass('visible');
+  });
+
+  flatLink.on('mouseout', function() {
+    flatsPath.removeClass('visible');
+  });
+
+  function setNumberFlat() {
+    for (let i = 0; i < numberFlats.length; i++) {
+      numberFlats[i].innerHTML = `${String(+currentFloor) + i}`
+    }
+  }
+
+  floorPath.on('click', function() {
+    toggleModal();
+    setNumberFlat();
+  });
+  
   modalCloseButton.on('click', toggleModal);
-  viewFlatsButton.on('click', toggleModal);
+
+  viewFlatsButton.on('click', function() {
+    toggleModal();
+    setNumberFlat();
+  });
 
   counterUp.on('click', function() {
     if (currentFloor >= 18) {
@@ -31,7 +70,6 @@ $(document).ready(function () {
       $('.counter').text(usCurrentFloor);
       floorPath.removeClass('current-floor');
       $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
-    
   })
   counterDown.on('click', function() {
     if (currentFloor <= 2) {
@@ -43,7 +81,11 @@ $(document).ready(function () {
       floorPath.removeClass('current-floor');
       $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
   });
+
   function toggleModal() {
     modal.toggleClass('is-open');
+    /*for (let i = 0; i < numberFlats.length; i++) {
+      numberFlats[i].innerHTML = `${String(+currentFloor) + i}`
+    }*/
   }
 });
